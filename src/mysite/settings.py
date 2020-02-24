2d267f7cc4b4e498916130b572ebb9144e7a48b3
yuenu <miraiku82@gmail.com>
2020-02-24 13:00:52 +0800

    5th commit

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATES_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL= 'blog-home'
