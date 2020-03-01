a7b7d7d8044f91e5dcf0f8ac7e0dbd6fee5a03f5
yuenu <miraiku82@gmail.com>
2020-03-01 18:40:24 +0800

    revise Environment Variables

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -137,15 +137,16 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'miraiku82@gmail.com'
-EMAIL_HOST_PASSWORD = 'Wak6hcjpw2'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_ACCESS_KEY_ID = 'AKIAS6HBELL5FGR3XBRD'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-AWS_STORAGE_BUCKET_NAME = 'django-image-files'
+
